75a06cbd8f1d90cb4aaf9e6ff8e73c8e374beac6
Jeremy <jejedech.63@gmail.com>
2023-06-04 13:04:23 +1000

    adding handlebars pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,21 @@ const { Blog, User } = require('../models');
 
 router.get('/', async (req,res) =>{
   res.render('homepage')
-})
+});
+
+router.get('/dashboard', async (req,res) =>{
+  res.render('dashboard')
+});
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
