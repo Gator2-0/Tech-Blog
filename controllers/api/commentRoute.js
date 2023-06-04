71308c4757229c0ac823f735efeff9414927a9b8
Jeremy <jejedech.63@gmail.com>
2023-06-04 16:40:53 +1000

    adding new comment feature

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -3,14 +3,30 @@ const { json } = require('sequelize');
 const { Comments } = require('../../models');
 
 router.post('/', (req,res) => {
+  console.log(req.body);
   try {
-    const commentData = Comments.create(req.body);
+    const request = {
+      "content" : req.body.comment,
+      "user_id" : req.session.user_id,
+      "blog_id" : req.body.blog_id
+    }
+    const commentData = Comments.create(request);
 
     res.status(200),json(commentData);
 
   } catch (err) {
     res.status(400).json(err);
   }
+})
+
+router.get('/', async (req,res) => {
+  try {
+    const commentData = await Comments.findAll();
+    res.status(200).json(commentData)
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 })
 
 module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,25 @@
 const commentFormHandler =async (event) => {
   event.preventDefault();
 
-  const comment = document.querySelector('#comment-text').ariaValueMax.trim();
+  const comment = document.querySelector('#comment-text').value.trim();
+  let url = document.location.href;
+  const blog_id = url.substring(url.lastIndexOf('/') + 1);
+  
 
   if(comment){
     const response = await fetch('/api/comments/', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ comment, blog_id }),
       headers: { 'Content-Type': 'application/json' },
     });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+
   }
 }
 
