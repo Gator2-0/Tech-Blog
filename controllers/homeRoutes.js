df9d4bad4bcee18203a46ef98d040c64473eff89
Jeremy <jejedech.63@gmail.com>
2023-06-04 15:24:31 +1000

    adding blog/:id feature

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,30 @@ router.get('/newBlog', withAuth, async (req,res) =>{
   })
 });
 
+router.get('/blog/:id', async (req,res) =>{
+  try {
+    const blogData = await Blog.findByPk(req.params.id,{
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blog = blogData.get({plain: true});
+    console.log('./blog/:id');
+    console.log(blog);
+
+    res.render('blog',{
+      ...blog,
+      logged_in: req.session.logged_in
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
